4506c3afe148ae9116fb01d447fbdf65b49700c0
biud436 <biud436@gmail.com>
2020-11-01 17:36:13 +0900

    가상 요소 선택자 추가

--- js/styleRules.js
+++ js/styleRules.js
@@ -1,9 +1,15 @@
 /**
  * 가상 요소는 자바스크립트로 선택할 수 없기 때문에 해당 스타일을 직접 찾아 변경합니다.
+ * 
+ * @author Eo Jinseok
+ * 
+ * @param {String} selector
+ * @param {String} property
+ * @param {String} value
  */
 export function cssRuleSet(selector, property, value) {
+    
     try {
-
         const root = document.styleSheets;
 
         for (let i = 0; i < root.length; i++) {
@@ -17,13 +23,14 @@ export function cssRuleSet(selector, property, value) {
                  * @type {CSSStyleSheet}
                  */
                 const styled = files[j].styleSheet;
+                
                 for(let rule of styled.rules) {
                     if(rule.selectorText === selector) {
                         rule.style.setProperty(property, value);
                     }
                 }
-
             }
+
         }
 
     } catch (e) {
