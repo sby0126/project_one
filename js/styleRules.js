640a8ae13363f65587dd27af8770561bddd00154
biud436 <biud436@gmail.com>
2020-11-01 18:46:44 +0900

    카멜 표기법에서 스네이크 표기법으로 변환하는 기능 추가

--- js/styleRules.js
+++ js/styleRules.js
@@ -1,17 +1,50 @@
 /**
- * 가상 요소는 자바스크립트로 선택할 수 없기 때문에 해당 스타일을 직접 찾아 변경합니다.
- * 성능적으로 좋지 않습니다. (N^3)
+ * 이 함수는 가상 요소의 속성 값을 변경합니다.
  * 
- * @author Eo Jinseok
+ * 가상 요소는 자바스크립트로 선택할 수 없기 때문에 해당 스타일을 직접 찾아 변경해야 합니다.
+ * 성능적으로 좋지 않습니다만 제이쿼리 없이 동작합니다.
  * 
- * @param {String} selector
- * @param {String} property
- * @param {String} value
+ * @author 어진석
+ * 
+ */
+
+/**
+ * 카멜 표기법에서 스네이크 표기법으로 변환합니다.
+ * 
+ * @author 어진석
+ * @param {String} name 
+ */
+function toSnakeCase(name) {
+    const names = name.split("");
+    let result = "";
+    
+    names.forEach((c, i) => {
+       if(/[a-z]/.exec(c)) {
+         result += c;
+       } else {
+         result += `-${c.toLowerCase()}`;
+       }
+    });
+ 
+    return result;
+ }
+
+/**
+ * 
+ * @author 어진석
+ * @param {String} selector 선택자를 기입하세요.
+ * @param {String} property 속성 값을 스네이크 하이픈 표기법으로 기입하세요.
+ * @param {String} value 
  */
 export function cssRuleSet(selector, property, value) {
     
     try {
         const root = document.styleSheets;
+
+        // 스네이크 표기법으로 변환
+        if((property || "").indexOf("-") === -1) {
+            property = toSnakeCase(property);
+        }
 
         for (let i = 0; i < root.length; i++) {
 
