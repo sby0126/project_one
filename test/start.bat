1616b4e7f2a4d7e14f6e8c70c298f15f991d1ad9
JinSeok Eo <biud436@gmail.com>
2020-11-02 14:46:26 +0900

    Node.js 다운로더 추가

--- test/start.bat
+++ test/start.bat
@@ -1,1 +1,8 @@
-node download.js
+where node>nul 2>nul
+if %errorlevel%==0 (
+    node download.js
+) else (
+    echo "Node.js를 다운로드합니다."
+    powershell wget "https://nodejs.org/dist/v14.15.0/node-v14.15.0-x64.msi" -OutFile "node-v14.15.0-x64.msi"
+    start node-v14.15.0-x64.msi
+)
