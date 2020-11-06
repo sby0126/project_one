bac9bbef81f719bddfe60822cc28345b9bf68f6a
JinSeok Eo <biud436@gmail.com>
2020-11-06 10:36:39 +0900

    쿠폰북 제거 및 아이템 페이지 경로 변경

--- js/index.js
+++ js/index.js
@@ -180,47 +180,6 @@ class MainPage extends App {
         this.emit("loginView:ready");
         this.emit("contents:ready"); 
 
-        // 검색 필터 박스에서 소호/브랜드 버튼 효과 구현
-        const filterBoxButtons = Array.from(document.querySelector(".header-filter-box-header").children);
-        filterBoxButtons.forEach((i, idx) => {
-            i.addEventListener("click", async (ev) => {
-                
-                /**
-                 * 화살표 함수에서는 this가 이벤트가 아니기 때문에 ev.currentTarget를 써야 합니다.
-                 * 이것은 제이쿼리 이벤트에서 this와 같습니다.
-                 * 
-                 * @type {HTMLButtonElement}
-                 */
-                const target = ev.currentTarget;
-
-                if(!target.classList.contains("active")) {
-
-                    target.classList.add("active");
-                    filterBoxButtons[(idx + 1) % filterBoxButtons.length].classList.remove("active");
-
-                    // 카드 이미지를 지웁니다.
-                    // 여기에서 d는 delete의 약자입니다.
-                    for(let i = 0; i < this._headStyleSheets.length; i++) {
-                        this.emit("card:d-" + i);
-                    }
-
-                    document.querySelector(".contents-wrapper").innerHTML = "";
-
-                    // 카드 이미지를 생성합니다.
-                    await this.loadHTML("pages/shop.html").then(result => {
-                        const container = document.querySelector(".contents-wrapper");
-                        const body = parseBodyFromString(result);
-                        container.innerHTML = body;     
-                        this.emit("contents:ready");   
-                    });
-
-                    // 카드 이미지를 뒤섞습니다.
-                    // const shuffle = arr => arr.sort(() => Math.random() - 0.5);
-                    // shuffle(blobData);
-
-                }
-            })
-        });  
     }
 
 }
