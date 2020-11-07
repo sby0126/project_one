8972bf59550792c667e0cdff10a0b749ad0c7efd
biud436 <biud436@gmail.com>
2020-11-07 14:35:22 +0900

    added Card Builder to main page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import {App} from "./app.js";
 import {LoginButton} from "./components/LoginButton.js";
 import { ShopContentLoader } from "./components/ShopContentLoader.js";
+import { CardStyleSheetBuilder } from "./components/CardStyleSheetBuilder.js";
 
 /**
  * ==============================================
@@ -30,68 +31,31 @@ class MainPage extends App {
         
     }
 
-    /**
-     * CSS를 자바스크립트에서 동적으로 생성합니다.
-     * 이 메소드는 가상 요소로 만든 둥근 이미지를 변경하기 위해 정의하였습니다.
-     * <p></p> 요소는 각각 특정 dataID를 attribute로 가집니다.
-     * 
-     * @link https://stackoverflow.com/a/524721
-     * @param {String} dataId 
-     * @param {String} imagePath 
-     */
     createNewStyleSheet(dataID, imagePath) {
-        const head = document.head || document.getElementsByTagName('head')[0];
-        const style = document.createElement('style');
 
-        head.appendChild(style);
-
-        const css = `.card p[${dataID}]::before {
-            content: "";
-            width: 5.5em;
-            height: 5.5em;
-            background: url("${imagePath}") center;
-            background-size: cover;
-            position: absolute;
-            border-radius: 50%;
-            left: calc(50% - 5.5em / 2);
-            top: 10%;
-            z-index: 0;
-        }
-
-        .card p[${dataID}]:hover::before {
-            filter: brightness(1.1);
-            border-radius: 0;
-            transition: all .2s linear;
-        }
+        CardStyleSheetBuilder.builder(this, `
         
-        `;
+            .card p[${dataID}]::before {
+                content: "";
+                width: 5.5em;
+                height: 5.5em;
+                background: url("${imagePath}") center;
+                background-size: cover;
+                position: absolute;
+                border-radius: 50%;
+                left: calc(50% - 5.5em / 2);
+                top: 10%;
+                z-index: 0;
+            }
 
-        if (style.styleSheet) {
-            // IE 8
-            style.styleSheet.cssText = css;
-        } else {
-            const child = document.createTextNode(css);
-            style.appendChild(child);
-            this._headStyleSheets.push(dataID);
-
-            // 삭제 이벤트를 정의합니다.
-            // 사용 예: 
-            //  app.emit("card:d-0"); // 1번 카드의 스타일을 지운다.
-            //  app.emit("card:d-1"); // 2번 카드의 스타일을 지운다.
-            this.on(`card:${dataID}`, () => {
-                const idx = this._headStyleSheets.indexOf(dataID);
-                delete this._headStyleSheets[idx];
-
-                // removeChild를 쓰면 메모리에 남아서 안지워짐.
-                style.remove(child);
-
-                // 등록된 이벤트를 제거합니다.
-                setTimeout(() => {
-                    this.off(`card:${dataID}`);
-                }, 0);
-            });
-        }
-    }    
+            .card p[${dataID}]:hover::before {
+                filter: brightness(1.1);
+                border-radius: 0;
+                transition: all .2s linear;
+            }
+        
+        `, dataID).run();
+    }
 
     addEventListeners() {
         this.on("login:ready", () => LoginButton.builder().run());
