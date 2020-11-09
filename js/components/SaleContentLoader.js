83e0d8db24bd2598cc2cf41c0df80ca15575dcd1
biud436 <biud436@gmail.com>
2020-11-10 00:12:28 +0900

    할인 페이지에 카드 3개씩 가져오는 기능 추가

--- js/components/SaleContentLoader.js
+++ js/components/SaleContentLoader.js
@@ -2,22 +2,36 @@ import { Component } from "./Component.js";
 import {saleData} from "../saleData.js";
 
 export class SaleContentLoader extends Component {
-    run() {
 
-        const items = Array.from(document.querySelectorAll(".card-container .card"));
+    initMembers(parent) {
+        super.initMembers(parent);
+
+        this._currentCards = 0;
+        this._fetchCards = 3;
+        this._maxCards = 50;
+        this._interval = 800;
+    }
+
+    appendCards() {
+        let currentCards = this._currentCards;
+        const fetchCards = this._fetchCards;
+        const maxCards = this._maxCards   
+        const max = Math.min(currentCards + fetchCards, maxCards);
+
         const parent = this._parent;
 
-        $(".card").css({
-            "flex-basis": "30%",
-            "height": "35rem",
-        });
+        for(let idx = currentCards; idx < (currentCards + fetchCards); idx++) {
+            const card = this._items[idx];
 
-        items.forEach((card, idx) => {
-            card.querySelector("p").setAttribute("d-"+idx, "");
-            
             let myImgData = saleData[idx];
 
+            if(!card) {
+                continue;
+            }           
+
             if(myImgData) {
+                card.querySelector("p").setAttribute("d-"+idx, "");
+
                 const filename = myImgData;
                 parent.createNewStyleSheet("d-"+idx, filename.url);     
 
@@ -32,8 +46,30 @@ export class SaleContentLoader extends Component {
                         <button class="like-button"></button>
                     </div>
                 `);
-            }
+            }            
+
+            if(this._currentCards < this._maxCards)
+                this._currentCards++;            
+        }        
+    }
+
+    run() {
+
+        this._items = Array.from(document.querySelectorAll(".card-container .card"));
+
+        $(".card").css({
+            "flex-basis": "30%",
+            "height": "35rem",
         });
+
+        this.appendCards();
+
+        const throttled  = _.throttle(() => {
+            this.appendCards();
+        }, this._interval);
+
+        $(window).scroll(throttled);        
+
     }
 
     static id() {
