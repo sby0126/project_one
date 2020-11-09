be977e3fd86067e12dbf7b0031abd490ed51304d
JinSeok Eo <biud436@gmail.com>
2020-11-09 12:06:39 +0900

    find-password.css 추가

--- scss/build.bat
+++ scss/build.bat
@@ -1,1 +1,2 @@
-npx sass --style expanded --source-map --embed-sources --no-error-css ./:css
+@REM npx sass --style expanded --source-map --embed-sources --no-error-css ./:../css
+npx sass --style expanded --no-source-map --no-error-css ./:../css
