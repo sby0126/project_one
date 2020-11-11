08c35faac7e7021fb05cf9f1763ffc79a5ec7b0f
JinSeok Eo <biud436@gmail.com>
2020-11-11 10:28:57 +0900

    json 파일 분리

---
+++ json/shop/core.js
@@ -0,0 +1,11 @@
+const blobData = require("./collection.json");
+
+const fs = require('fs');
+const path = require('path');
+
+
+for(let i = 0; i < blobData.length; i++) {
+    const json = blobData[i];
+    const filepath = path.join(path.resolve(__dirname), `shop_data${i}.json`);
+    fs.writeFileSync(filepath, JSON.stringify(json), "utf8");
+}
