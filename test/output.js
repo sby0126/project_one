20113a5dafa4db8b5af82ad142cbf4ca80e7d69f
biud436 <biud436@gmail.com>
2020-11-11 22:41:13 +0900

    sale 페이지에 필요한 데이터 생성

--- test/output.js
+++ test/output.js
@@ -1,8 +1,14 @@
-const {shop} = require("../json/prebuilt_data.json");
+/**
+ * @author 어진석
+ * @description
+ * 호스트 명과 자세한 경로를 지우고 파일명만 남깁니다.
+ */
+
+const {sale} = require("../json/prebuilt_data.json");
 const path = require('path');
 const fs = require('fs');
 
-const output = shop.map(i => {
+const output = sale.map(i => {
     const raw = i.src.replace(/\?(.*)/g, "");
     const filename = path.basename(raw);
     i.url = filename;
