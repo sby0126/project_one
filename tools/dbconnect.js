e20c5fa07470787096f8fb9c75c957459ec3d14d
JinSeok Eo <biud436@gmail.com>
2020-11-17 13:07:43 +0900

    오라클 DB 연동 테스트 #26

---
+++ tools/dbconnect.js
@@ -0,0 +1,29 @@
+/**
+ * @description
+ * 오라클 DB 연동
+ * 
+ * 데이터 삽입, 조회 가능
+ */
+const oracledb = require("oracledb");
+
+oracledb.getConnection({
+    user: "scott",
+    password: "tiger",
+    host: "localhost",
+    database: "orcl",
+}, (err, conn) => {
+    if(err) {
+        console.log("DB 접속 실패!")
+        return;
+    }
+    console.log("DB 접속 성공!!");
+
+    conn.execute("select * from emp", {}, {outFormat: oracledb.OUT_FORMAT_OBJECT}, (err, result) => {
+        if(err) {
+            throw new Error(err);
+        }
+
+        console.log(result);
+    });
+
+});
