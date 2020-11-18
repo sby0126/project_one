5b79f11fe5bd374f5d6c7120773a9b8cb6ab985a
biud436 <biud436@gmail.com>
2020-11-18 23:29:33 +0900

    게시판 REST API 추가

---
+++ src/Board.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/*")
+public class Board extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json;charset=utf-8;");
+		
+		PrintWriter out = response.getWriter();
+		
+		String reqUrl = request.getRequestURI();
+		String mappingUrl = request.getServletPath();
+		String pathInfo = request.getPathInfo();
+		
+		String postNumber = pathInfo.substring(1);
+		
+		out.print("{\"pathInfo\": \"" + pathInfo + ",");
+		out.print("\"content\": \"" + "Å×½ºÆ®" + "\",");
+		out.print("\"postNumber\": \"" + postNumber + "\"}");
+	}
+
+}
