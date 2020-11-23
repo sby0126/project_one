bd5592f7a2f03462c10d98562d616cd5783bef97
biud436 <biud436@gmail.com>
2020-11-24 00:40:04 +0900

    아이디 중복 여부 판단 쿼리문 추가

--- src/core/CustomerController.java
+++ src/core/CustomerController.java
@@ -2,6 +2,8 @@ package core;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -43,12 +45,16 @@ public class CustomerController extends HttpServlet {
 		String nextPage = null;
 		
 		if(act == null || act.equals("/members.do")) {
+			
 			List<CustomerVO> customerList = customerDAO.listMembers();  
 			request.setAttribute("customerList", customerList);		
 			nextPage = "/pages/members.jsp";
+			
 		} else if(act.equals("/signUp.do")) {
+			
+			// 이걸 줄이려면 마이바티스라는 프레임워크를 사용해야 한다.
 			String id = request.getParameter("id");
-			String hashedPassword = request.getParameter("password");
+			String password = request.getParameter("password");
 			String no = request.getParameter("no");
 			String name = request.getParameter("name");
 			String address = request.getParameter("address");
@@ -57,21 +63,45 @@ public class CustomerController extends HttpServlet {
 			String isAdmin = "N";
 			String joinDate = request.getParameter("joinDate");
 			
-			CustomerVO c = new CustomerVO();
+			boolean isValid = true;
 			
-			c
-				.setId(id)
-				.setPassword(hashedPassword)
-				.setNo(no)
-				.setName(name)
-				.setAddress(address)
-				.setTel(tel)
-				.setEmail(email)
-				.setIsAdmin(isAdmin)
-				.setJoinDate(joinDate);		
+			// ID 중복 여부 체크
+			if(customerDAO.isUniqueMember(id)) {
+				// 회원 가입 실패 처리
+				request.setAttribute("errorMessage", "해당 아이디는 이미 사용 중입니다.");
+				nextPage = "/pages/error.jsp";
+				isValid = false;
+			}
 			
-			customerDAO.addCustomer(c);
-			nextPage = "/pages/members.jsp";
+			// 비밀번호 유효성 검사
+			if(!isValidPassword(password)) {
+				// 회원 가입 실패 처리
+				request.setAttribute("errorMessage", "특수 문자나 영어 및 대 소문자 섞어야 합니다");
+				nextPage = "/pages/error.jsp";
+				isValid = false;
+			}
+			
+			// 이메일 중복 여부 체크
+			
+			
+			if(isValid) {
+				CustomerVO c = new CustomerVO();
+				
+				c
+					.setId(id)
+					.setPassword(password)
+					.setNo(no)
+					.setName(name)
+					.setAddress(address)
+					.setTel(tel)
+					.setEmail(email)
+					.setIsAdmin(isAdmin)
+					.setJoinDate(joinDate);		
+				
+				customerDAO.addCustomer(c);
+				nextPage = "/pages/members.jsp"; // 회원 가입 환영 또는 최신 멤버 목록으로 이동 				
+			}
+
 		} else {
 			response.sendRedirect("/");
 		}
@@ -79,5 +109,26 @@ public class CustomerController extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
 		dispatcher.forward(request, response);
 	}
+	
+	
+	/**
+	 * 하나 이상의 알파벳을 포함해야 함
+	 * 하나 이상의 숫자를 포함해야 함
+	 * 하나 이상의 특수문자를 포함해야 함
+	 * 최소 8글자 이상 입력해야 함
+	 * 
+	 * @link https://minwoohi.tistory.com/98
+	 * @param password
+	 * @return
+	 */
+	public boolean isValidPassword(String password) {
+		
+		String passwordPolicy = "((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9가-힣]).{8,})";
+		
+		Pattern pattern = Pattern.compile(passwordPolicy);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+	}	
+		
 
 }
--- src/core/CustomerDAO.java
+++ src/core/CustomerDAO.java
@@ -72,6 +72,73 @@ public class CustomerDAO {
 		return customerList;
 	}
 	
+	/**
+	 * 아이디 중복 여부 판단 (쿼리문 작성)
+	 * 
+	 * 해당 아이디의 멤버가 조회되지 않는다면 유일한 아이디로 판단한다.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public boolean isUniqueMember(String id) {
+		
+		List<CustomerVO> customerList = new ArrayList<CustomerVO>();
+		String query = "select * from tblCustomer where CTMID = ?";
+		
+		boolean isUnique = false;
+				
+		try {
+			conn = dataFactory.getConnection();
+			pstmt = conn.prepareStatement(query);	
+			
+			pstmt.setString(1, id);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String _id = rs.getString("CTMID");
+				String hashedPassword = rs.getString("CTMPW");
+				String no = rs.getString("CTMNO");
+				String name = rs.getString("CTMNM");
+				String address = rs.getString("ADDR");
+				String tel = rs.getString("TEL");
+				String email = rs.getString("EMAIL");
+				String isAdmin = rs.getString("isAdmin");
+				String joinDate = rs.getString("joinDate");
+				
+				CustomerVO c = new CustomerVO();
+				
+				c
+					.setId(_id)
+					.setPassword(hashedPassword)
+					.setNo(no)
+					.setName(name)
+					.setAddress(address)
+					.setTel(tel)
+					.setEmail(email)
+					.setIsAdmin(isAdmin)
+					.setJoinDate(joinDate);
+				
+				customerList.add(c);
+			}
+			
+			if(customerList == null ) {
+				isUnique = true; // 유일한 아이디
+			} else {
+				isUnique = false; // 아이디가 이미 존재한다.
+			}
+			
+			rs.close();
+			pstmt.close();
+			conn.close();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return isUnique;
+	}
+	
 	public void addCustomer(CustomerVO c) {
 		try {
 			
