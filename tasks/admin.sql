2d1e9f602e03b22f814fbe013e5edd704ed4c5f0
biud436 <biud436@gmail.com>
2020-11-24 22:51:14 +0900

    admin.sql, customer.sql 수정

--- tasks/admin.sql
+++ tasks/admin.sql
@@ -1,5 +1,6 @@
 -- DB 연결
 conn sys as sysdba;
+conn system/admin;
 
 -- 앞에 c##을 안붙일 수 있게 수정
 alter session set "_ORACLE_SCRIPT"=true;
@@ -7,3 +8,5 @@ alter session set "_ORACLE_SCRIPT"=true;
 -- 계정 생성 및 권한 부여
 create user admin identified by admin;
 GRANT CONNECT, RESOURCE TO admin;
+
+alter user admin default tablespace users quota unlimited on users;
--- tasks/customer.sql
+++ tasks/customer.sql
@@ -2,13 +2,13 @@
 -- 고객 테이블
 --*****************************************************************************
 create table tblCustomer (
-    CTMID            VARCHAR2(15),                  -- ID (PK)
-    CTMPW            VARCHAR2(64),                  -- 암호화된 비밀번호 (64자리)
+    CTMID            VARCHAR2(15) NOT NULL,         -- ID (PK)
+    CTMPW            VARCHAR2(64) NOT NULL,         -- 암호화된 비밀번호 (64자리)
     CTMNO            NUMBER(8) NOT NULL,            -- 번호
     CTMNM            VARCHAR(15) NOT NULL,          -- 이름
-    ADDR             VARCHAR2(30) NOT NULL,         -- 주소
+    ADDR             VARCHAR2(30),                  -- 주소
     TEL              VARCHAR2(15),                  -- 전화번호
-    EMAIL            VARCHAR2(20) NOT NULL,         -- 이메일
+    EMAIL            VARCHAR2(30) NOT NULL,         -- 이메일
     ZIPCODE          CHAR(5),                       -- 새로운 우편번호 (5자리)
     IS_ADMIN         CHAR(1) DEFAULT 'N',           -- 관리자 여부 (Y/N)
     JOINDATE         DATE NOT NULL,                 -- 가입일
@@ -20,3 +20,13 @@ create table tblCustomer (
 
 -- 게시판 테이블과 연동되어야 하므로 기본키를 ID로 설정
 ALTER TABLE tblCustomer add CONSTRAINT tblCustomer_CTMID_pk PRIMARY KEY(CTMID);
+
+-- 시퀀스
+CREATE SEQUENCE CUSTNO_SEQ 
+INCREMENT BY 1
+START WITH 1
+MINVALUE 1
+NOCYCLE;
+
+INSERT INTO tblCustomer (CTMID, CTMPW, CTMNO, CTMNM, EMAIL, IS_ADMIN, JOINDATE, SALT) 
+    VALUES('admin', 'admin', CUSTNO_SEQ.NEXTVAL, '관리자', 'admin@projectone.co.kr', 'Y', sysdate, 'AAAABBBBCCCCDDDD');
