a704b860afc66c8e52faee503de2e5963d9d74a2
sby0126 <73582669+sby0126@users.noreply.github.com>
2020-12-01 18:16:17 +0900

    up
    a

--- src/core/board/notice/BoardPostServlet.java
+++ src/core/board/notice/BoardPostServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BoardPostServlet
  */
-@WebServlet("/boardPost.do")
+@WebServlet("/boardPost")
 public class BoardPostServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
