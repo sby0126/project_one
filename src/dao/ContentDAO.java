d689d65e38e58e79aab4acdacc54431cebdb0a3d
biud436 <biud436@gmail.com>
2020-12-09 00:03:45 +0900

    불필요한 주석을 제거했습니다.

--- src/dao/ContentDAO.java
+++ src/dao/ContentDAO.java
@@ -20,23 +20,6 @@ public class ContentDAO implements IDAO {
 	private static ContentDAO instance = null;
 	private ContentLoader qlList = null;
 	
-//	class ContentParams {
-//		public int startNumber;
-//		public int endNumber;
-//		String pageType;
-//		String shopType;
-//		String gndr;
-//		
-//		public ContentParams(int startNumber, int endNumber, String pageType, String shopType, String gndr) {
-//			this.startNumber = startNumber;
-//			this.endNumber = endNumber;
-//			this.pageType = pageType;
-//			this.shopType = shopType;
-//			this.gndr = gndr;
-//		}
-//		
-//	}
-	
 	private ContentDAO() {
 		create();
 	}
