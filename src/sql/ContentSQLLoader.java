38b2a2e5beddec080bca1c4c5089bfe07eafe18e
JinSeok Eo <biud436@gmail.com>
2020-12-08 14:35:16 +0900

    added ContentSQLLoader

---
+++ src/sql/ContentSQLLoader.java
@@ -0,0 +1,25 @@
+package sql;
+
+import java.util.HashMap;
+
+public class ContentSQLLoader {
+	private HashMap<String, String> qlList;
+	
+	public ContentSQLLoader() {
+		qlList = new HashMap<String, String>(); 
+	}
+	
+	public void initWithSQL() {
+		qlList.put("search", 
+				"SELECT DISTINCT b.pageType, b.genderType, b.shopType, b.shopName, a.imgId"
+						+ " FROM tblimagehash a, tblproduct b"
+						+ " WHERE a.pageType = ? AND"
+						+ " a.genderType = ? AND"
+						+ " a.shopType = ? AND"
+						+ " a.imgUrl = b.contentUrl");
+	}
+	
+	public String get(String command) {
+		return qlList.get(command);
+	}
+}
