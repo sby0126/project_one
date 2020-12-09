29a2b0b03857b81f4c1001ecb5f3fb6d15e82f08
JinSeok Eo <biud436@gmail.com>
2020-12-09 12:47:59 +0900

    전체 데이터 추출로 변경

--- src/sql/ContentLoader.java
+++ src/sql/ContentLoader.java
@@ -13,30 +13,20 @@ public class ContentLoader {
 	
 	public void initWithSQL() {
 		
-		qlList.put("전체 데이터 검색", "SELECT DISTINCT b.pageType, b.genderType, b.shopType, b.shopName, b.contentUrl, a.imgId, b.*"
+		qlList.put("전체 데이터 추출", "SELECT DISTINCT b.pageType, b.genderType, b.shopType, b.shopName, b.contentUrl, a.imgId, b.*"
 						+ " FROM tblimagehash a, tblproduct b"
 						+ " WHERE a.pageType = ? AND"
 						+ " a.genderType = ? AND"
 						+ " a.shopType = ? AND"
 						+ " a.imgUrl = b.contentUrl");
 		
-		qlList.put("나이 및 카테고리로 찾기", "SELECT DISTINCT b.pageType, b.genderType, b.shopType, b.shopName, b.contentUrl, a.imgId, b.*"
-					+ " FROM tblimagehash a, tblproduct b"
-					+ " WHERE a.pageType = ? AND"
-					+ " a.genderType = ? AND"
-					+ " a.shopType = ? AND"
-					+ " a.imgUrl = b.contentUrl AND"
-					+ " texts LIKE ? AND"
+		qlList.put("나이 및 카테고리로 필터링", qlList.get("전체 데이터 추출")
+					+ " AND texts LIKE ? AND"
 					+ " texts LIKE ?"
 				);
 		
-		qlList.put("나이 또는 카테고리 검색", "SELECT DISTINCT b.pageType, b.genderType, b.shopType, b.shopName, b.contentUrl, a.imgId, b.*"
-				+ " FROM tblimagehash a, tblproduct b"
-				+ " WHERE a.pageType = ? AND"
-				+ " a.genderType = ? AND"
-				+ " a.shopType = ? AND"
-				+ " a.imgUrl = b.contentUrl AND"
-				+ " texts LIKE ?"
+		qlList.put("나이 또는 카테고리로 필터링", qlList.get("전체 데이터 추출")
+				+ " AND texts LIKE ?"
 			);	
 		
 	}
