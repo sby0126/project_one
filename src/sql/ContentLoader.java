b4df2ab66d3c46d81abc5cdb877a9c4d8e2469b6
JinSeok Eo <biud436@gmail.com>
2020-12-09 13:02:23 +0900

    특정 범위의 데이터 추출하는 SQL 문 추가

--- src/sql/ContentLoader.java
+++ src/sql/ContentLoader.java
@@ -29,6 +29,18 @@ public class ContentLoader {
 				+ " AND texts LIKE ?"
 			);	
 		
+		qlList.put("전체 데이터 번호 붙여 추출", "SELECT * FROM (SELECT DISTINCT ROW_NUMBER() OVER w AS 'row_number', b.pageType, b.genderType, b.shopType, b.shopName, b.contentUrl, a.imgId, b.texts, b.title, b.price, b.term"
+					+ " FROM tblimagehash a, tblproduct b"
+					+ " WHERE a.pageType = ? AND"
+					+ " a.genderType = ? AND"
+					+ " a.shopType = ? AND"
+					+ " a.imgUrl = b.contentUrl"
+					+ " WINDOW w AS (order BY id ASC)) AS mytbl"
+				);
+		
+		qlList.put("특정 범위의 데이터만 추출", qlList.get("전체 데이터 번호 붙여 추출") 
+					+ " WHERE mytbl.row_number BETWEEN ? AND ?"
+				);
 	}
 	
 	public String get(String command) {
