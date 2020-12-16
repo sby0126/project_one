c69d215ef995a978d6b5a275cc25025a2bff6c51
JinSeok Eo <biud436@gmail.com>
2020-12-16 13:03:59 +0900

    게시물 추천 기능 추가 완료

--- src/command/IncreaseRecommandCountCommand.java
+++ src/command/IncreaseRecommandCountCommand.java
@@ -1,10 +1,12 @@
 package command;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import action.ActionResult;
 
@@ -16,16 +18,35 @@ public class IncreaseRecommandCountCommand extends Command {
 		result.start(request, response);
 		
 		int postNumber = Integer.parseInt( request.getParameter("postNumber") );
-		String receiverId = request.getParameter("receiverId");
+		HttpSession session = request.getSession();
+		String receiverId = (String)session.getAttribute("id");
+		
+		if(receiverId == null) {
+			
+			response.setCharacterEncoding("text/html; charset=utf-8");
+			request.setAttribute("_status", "error");
+			request.setAttribute("errorMessage", "로그인이 되어있지 않습니다.");
+			request.setAttribute("url", "/pages/board-default.jsp");
+			
+			result.forward("/pages/error.jsp");
+			
+			return result;
+		}
 		
 		boolean isSuccess = getDAO().increaseRecommendCount(postNumber, receiverId);
 		
 		if(isSuccess) {
-			result.forward("/pages/boardPost.jsp");
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.println("success");
+			
+			return null;
+			
 		} else {
-			result = null;
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.println("duplicate");
+			return null;
 		}
-		
-		return result;
 	}
 }
