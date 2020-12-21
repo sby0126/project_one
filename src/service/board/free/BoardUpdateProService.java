5d8c16f9239897cff77141c34192845342c4e408
Handeokho <73689856+Handeokho@users.noreply.github.com>
2020-12-21 23:59:21 +0900

    게시판 에디터
    게시판 에디터

--- src/service/board/free/BoardUpdateProService.java
+++ src/service/board/free/BoardUpdateProService.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 
 import dao.board.free.BoardDAODeok;
 import vo.board.free.BoardBean;
+import vo.board.free.Re_boardBean;
 
 public class BoardUpdateProService {
 	public boolean updateArticle(BoardBean article) throws Exception{
@@ -19,6 +20,28 @@ public class BoardUpdateProService {
 	      BoardDAODeok boardDAO = BoardDAODeok.getInstance();
 	      boardDAO.setConnection(con);
 	      int insertCount = boardDAO.updateArticle(article);
+	      
+	      if(insertCount > 0){
+	         commit(con);
+	         updateSuccess = true;
+	      }
+	      else{
+	         rollback(con);
+	      }
+	      
+	      close(con);
+	      return updateSuccess;
+	      
+	   }
+	
+	public boolean updateReArticle(Re_boardBean re_article) throws Exception{
+	      // TODO Auto-generated method stub
+	      
+	      boolean updateSuccess = false;
+	      Connection con = getConnection();
+	      BoardDAODeok boardDAO = BoardDAODeok.getInstance();
+	      boardDAO.setConnection(con);
+	      int insertCount = boardDAO.updateReArticle(re_article);
 	      
 	      if(insertCount > 0){
 	         commit(con);
