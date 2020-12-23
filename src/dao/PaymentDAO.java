5016ba8c9607f80e8bff04f352d78e293dff9d61
JinSeok Eo <biud436@gmail.com>
2020-12-23 13:20:44 +0900

    getPayment 함수 추가

--- src/dao/PaymentDAO.java
+++ src/dao/PaymentDAO.java
@@ -2,8 +2,11 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
+import core.SQLHelper;
 import utils.DBConnectionMgr;
 import vo.PaymentVO;
 
@@ -73,6 +76,36 @@ public class PaymentDAO implements IDAO {
 		
 		return isOK;
 		
+	}
+	
+	public PaymentVO getPayment(String imp_uid) {
+		boolean isOK = false;
+		ResultSet rs = null;
+		String query = "select * from tblPayment where imp_uid = ?";
+		PaymentVO payment = null;
+		
+		try {
+			conn = pool.getConnection();
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, imp_uid);
+			
+			rs = pstmt.executeQuery();
+			
+			List<PaymentVO> list = SQLHelper.putResult(rs, PaymentVO.class);
+			
+			if(rs.next()) {
+				payment = list.get(0);
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(conn, pstmt);
+		}
+		
+		return payment;
 	}
 	
 }
